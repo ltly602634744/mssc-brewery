ef037a9099d4aa9bd06e2859eaaaa9686a5dc168
ltly602634744 <yan.liyan1204@gmail.com>
2020-07-22 20:19:31 -0400

    v2

--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -1,11 +1,13 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class BeerServiceImpl implements BeerService {
     @Override
     public BeerDto getBeerById(UUID beerId) {
@@ -13,5 +15,22 @@ public class BeerServiceImpl implements BeerService {
                 .beerName("Galaxy Cat")
                 .beerStyle("Pale Ale")
                 .build();
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        //todo impl - would abc a real impl to update beer
+    }
+
+    @Override
+    public void delete(UUID beerId) {
+        log.debug("Deleting a beer ...");
     }
 }
---
+++ src/main/java/guru/springframework/msscbrewery/services/v2/BeerServiceImplV2.java
@@ -0,0 +1,40 @@
+package guru.springframework.msscbrewery.services.v2;
+
+import guru.springframework.msscbrewery.services.BeerService;
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2;
+import guru.springframework.msscbrewery.web.model.v2.BeerStyleEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class BeerServiceImplV2 implements BeerServiceV2 {
+    @Override
+    public BeerDtoV2 getBeerById(UUID beerId) {
+        return BeerDtoV2.builder().id(UUID.randomUUID())
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyleEnum.ALE)
+                .build();
+    }
+
+    @Override
+    public BeerDtoV2 saveNewBeer(BeerDtoV2 beerDto) {
+        return BeerDtoV2.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDtoV2 beerDto) {
+        //todo impl - would abc a real impl to update beer
+    }
+
+
+    @Override
+    public void delete(UUID beerId) {
+        log.debug("Deleting a beer ...");
+    }
+}
